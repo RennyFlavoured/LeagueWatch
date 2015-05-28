7e180a0b00bd7c1f6442a360099e4c36fbc31dee
Stephen Rennie <stephen.rennie@tag-games.com>
2015-05-28 17:20:25 +0100

    Why is there so many...

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -9,8 +9,23 @@ class HomeController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $request = $this->getRequest();
-        file_put_contents('/tmp/file.txt', 'bob');
+        $name = $this->_getParam('name');
+        $apiKey = Zend_Registry::get('config')->api_key;
+
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER  => 1,
+            CURLOPT_URL             => sprintf('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/%s?api_key=%s', $name, $apiKey),
+            CURLOPT_USERAGENT       => 'LeagueWatch API'
+        ));
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        $this->view->resp = $resp;
+
     }
 
     public function populateAction()
